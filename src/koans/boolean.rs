c7e09e4f168ba586d38d05d8ae456cde0fb4463b
Simon <simon@bitcartel.com>
2019-04-07 17:20:53 -0700

    Solved boolean.rs

--- src/koans/boolean.rs
+++ src/koans/boolean.rs
@@ -9,18 +9,18 @@ fn truth() {
 // The != operator can be used to return true for an inequality
 #[test]
 fn falsehood() {
-    assert!(false __ true);
+    assert!(false != true);
 }
 
 // Strings can also be compared and will return a boolean
 #[test]
 fn string_equality() {
-    assert!("Stuff" == __);
+    assert!("Stuff" == "Stuff");
 }
 
 // Integers can be compared as long as they are of the same type
 #[test]
 fn int_equality() {
     let num: i8 = 5;
-    assert!(num == __);
+    assert!(num == 5);
 }
